3bf79dd6344440311c9ced49934761e2f811d752
Albert F. Garrow III <SirAlbertGarrow@gmail.com>
2015-11-23 04:12:13 -0500

    Fixed New Grid Placement Issue

--- application.js
+++ application.js
@@ -8,43 +8,36 @@ $(document).ready(function() {
 });
 
 $(document).ready(function(){
-	$("button").click(function(){
-    	$(".gridSquare").css("background-color", "white");
-        var width = prompt("Enter a width for the new grid.");
-
-        if(width > 90){
-            newGridWidthError();
-        }else{
-            var height = prompt("Enter a height for the new grid.");
-
-            if(height > 40){
-                newGridHeightError();
-            }
-
-            $(".gridSquare").remove();
-
-            for(var x=0; x<width; x++){
-                for(var y=0; y<height; y++){
-                    $("body").append("<div class='gridSquare'>bar</div>");
-                }
-            $("body").append("<br>");
-            }
-        }
-
-    });
-});
-
-$(document).ready(function(){
-    $(".gridSquare").hover(function(){
+    $(document).on("mouseenter", ".gridSquare", function(){
         $(this).css("background-color", "blue");
     });
 });
 
+$(document).ready(function(){
+	$("button").click(function(){
+        newGrid();
+    });
+});
 
-function newGridWidthError(){
-    alert("Width needs to be <=90");
-}
+function newGrid(){
+    $(".gridSquare").css("background-color", "white");
+    
+    var width = prompt("Enter a number that is <=90 for the width of the new grid.");
+    
+    if(width > 90){
+        alert("That number is greater than 90.");
+    }else{
+        var height = prompt("Enter a number that is <=40 for the height of the new grid.");
 
-function newGridHeightError(){
-    alert("Height needs to be <=40");
+        if(height > 40){
+            alert("That number is greater than 40.");
+        }else{
+            for(var x=0; x<width; x++){
+                for(var y=0; y<height; y++){
+                    $("body").append("<div class='gridSquare'>foo</div>");
+                }
+                $("body").append("<br>");
+            }
+        }
+    }
 }
